d86a0df67bbe0d66c75c30a5a1cb086afc73d831
Bovindu <87230990+Bovindu@users.noreply.github.com>
2025-06-05 19:30:54 +0530

    Updated leaderboard-data.ts

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -5,49 +5,49 @@ export const leaderboardData: Contestant[] = [
   {
     id: 1,
     name: "A K M Fowzin",
-    hours: 137,
+    hours: 37,
     profilePic: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     rank: 1
   },
   {
     id: 2,
     name: "A R Withana",
-    hours: 124,
+    hours: 24,
     profilePic: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     rank: 2
   },
   {
     id: 3,
     name: "D S I Manamperi",
-    hours: 116,
+    hours: 16,
     profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     rank: 3
   },
   {
     id: 4,
     name: "G Wasantha",
-    hours: 103,
+    hours: 13,
     profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     rank: 4
   },
   {
     id: 5,
     name: "H H C H Ediriweera",
-    hours: 98,
+    hours: 8,
     profilePic: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     rank: 5
   },
   {
     id: 6,
     name: "K A A Priyanga",
-    hours: 92,
+    hours: 7,
     profilePic: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     rank: 6
   },
   {
     id: 7,
     name: "M R D Perera",
-    hours: 89,
+    hours: 5,
     profilePic: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     rank: 7
   },
