59b80f4143eee72c140bead6da771fec4439d72e
Bovindu <87230990+Bovindu@users.noreply.github.com>
2025-06-05 16:52:08 +0530

    Updated TopContestants.tsx

--- src/components/TopContestants.tsx
+++ src/components/TopContestants.tsx
@@ -66,8 +66,7 @@ const TopContestants: React.FC<TopContestantsProps> = ({ contestants }) => {
               <h3 className={`font-bold text-center mt-2 ${isFirst ? "text-xl" : "text-lg"}`}>{contestant.name}</h3>
               
               <div className="flex items-center mt-2">
-                <span className={`${isFirst ? "text-3xl" : "text-2xl"} font-bold text-blue-600`}>{contestant.hours}</span>
-                <span className="ml-1 text-gray-600">hours</span>
+                <span className={`${isFirst ? "text-3xl" : "text-2xl"} font-bold text-blue-600`}>Rs. {contestant.hours*100}</span>
               </div>
               <div className="flex items-center mt-2">
                 <span className={`${isFirst ? "text-3xl" : "text-2xl"} font-bold text-blue-600`}>{contestant.hours}</span>
