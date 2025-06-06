1596e82ebcdd8f05ceac4950e5f794ca7a3a6ff1
Bovindu <87230990+Bovindu@users.noreply.github.com>
2025-06-06 10:44:41 +0530

    Updated leaderboard-data.ts

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -4,14 +4,14 @@ import fowzinImage from '../img/kobe.png';
 // Mock data for the leaderboard
 export const leaderboardData: Contestant[] = [
   {
-    id: 1,
+    id: 2,
     name: "A K M Fowzin",
     hours: 27,
     profilePic: fowzinImage,
     rank: 2
   },
   {
-    id: 2,
+    id: 1,
     name: "A R Withana",
     hours: 20,
     profilePic: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
