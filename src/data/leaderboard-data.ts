728b66d00d9877a80a4f426bf35bea2e9c5ea979
bovin <bovin@DESKTOP-1KSL3R7>
2025-06-08 11:06:08 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -4,11 +4,11 @@ import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
   { rank: 1, name: "G Wasantha", hours: 31, profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 2, name: "A K M Fowzin", hours: 28, profilePic: fowzinImage },
-  { rank: 3, name: "A R Withana", hours: 20, profilePic: WithanaImage },
-  { rank: 4, name: "D S I Manamperi", hours: 16, profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 5, name: "H H C H Ediriweera", hours: 8, profilePic: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 6, name: "K A A Priyanga", hours: 7, profilePic: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 7, name: "M R D Perera", hours: 5, profilePic: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 8, name: "Bovindu", hours: 3, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/e_blur:300/v1749315471/asd_sggs72.jpg" },
+  { rank: 2, name: "Bovindu", hours: 31, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/e_blur:300/v1749315471/asd_sggs72.jpg" },
+  { rank: 3, name: "A K M Fowzin", hours: 28, profilePic: fowzinImage },
+  { rank: 4, name: "A R Withana", hours: 20, profilePic: WithanaImage },
+  { rank: 5, name: "D S I Manamperi", hours: 16, profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+  { rank: 6, name: "H H C H Ediriweera", hours: 8, profilePic: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+  { rank: 7, name: "K A A Priyanga", hours: 7, profilePic: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+  { rank: 8, name: "M R D Perera", hours: 5, profilePic: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
 ];
