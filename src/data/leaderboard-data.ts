d9a8837d3d590b099fa1ba4942da35c7eadd577a
bovin <bovin@DESKTOP-1KSL3R7>
2025-06-08 11:16:10 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -3,8 +3,8 @@ import fowzinImage from '../img/kobe.png';
 import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
-  { rank: 1, name: "G Wasantha", hours: 31, profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 2, name: "Bovindu", hours: 31, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/e_blur:300/v1749315471/asd_sggs72.jpg" },
+  { rank: 1, name: "Bovindu", hours: 70, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/e_blur:300/v1749315471/asd_sggs72.jpg" },
+  { rank: 2, name: "G Wasantha", hours: 31, profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
   { rank: 3, name: "A K M Fowzin", hours: 28, profilePic: fowzinImage },
   { rank: 4, name: "A R Withana", hours: 20, profilePic: WithanaImage },
   { rank: 5, name: "D S I Manamperi", hours: 16, profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
