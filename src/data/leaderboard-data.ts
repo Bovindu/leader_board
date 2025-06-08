bdc520172c0631c7ef9b97a91039d84e42a82b5b
bovin <bovin@DESKTOP-1KSL3R7>
2025-06-08 11:31:39 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -4,7 +4,7 @@ import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
   { rank: 1, name: "Bovindu", hours: 70, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/e_blur:300/v1749315471/asd_sggs72.jpg" },
-  { rank: 2, name: "A K M Fowzin", hours: 48, profilePic: fowzinImage },
+  { rank: 2, name: "A K M Fowzin", hours: 38, profilePic: fowzinImage },
   { rank: 3, name: "G Wasantha", hours: 31, profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
   { rank: 4, name: "A R Withana", hours: 20, profilePic: WithanaImage },
   { rank: 5, name: "D S I Manamperi", hours: 16, profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
