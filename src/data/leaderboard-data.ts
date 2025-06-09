07b31d1beb4a2bbb9c8b24d13e44ffef4db22145
bovin <bovin@DESKTOP-1KSL3R7>
2025-06-09 09:22:34 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -4,7 +4,7 @@ import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
   { rank: 1, name: "D S I Manamperi", hours: 76, money: 1600, profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 2, name: "Bovindu", hours: 70, money: 7000, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/e_blur:300/v1749315471/asd_sggs72.jpg" },
+  { rank: 2, name: "Bovindu", hours: 60, money: 6000, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/e_blur:300/v1749315471/asd_sggs72.jpg" },
   { rank: 3, name: "A K M Fowzin", hours: 38, money: 3800, profilePic: fowzinImage },
   { rank: 4, name: "G Wasantha", hours: 31, money: 3100, profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
   { rank: 5, name: "A R Withana", hours: 10, money: 1000, profilePic: WithanaImage },
