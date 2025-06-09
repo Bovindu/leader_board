2f79edb5e1d158c25f7b4fa195bbba87b07e248d
hr.info <hr.info@HP-SRVEY2>
2025-06-09 11:40:50 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -7,8 +7,7 @@ export const leaderboardData = [
   { rank: 2, name: "D S I Manamperi", hours: 76, money: 1600, profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
   { rank: 3, name: "A K M Fowzin", hours: 38, money: 3800, profilePic: fowzinImage },
   { rank: 4, name: "G Wasantha", hours: 31, money: 3100, profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 5, name: "Tharindu", hours: 30, money: 3000, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1749441414/lebron_ylttqb.jpg" },
-  { rank: 6, name: "A R Withana", hours: 10, money: 1000, profilePic: WithanaImage },
-  { rank: 7, name: "K A A Priyanga", hours: 7, money: 700, profilePic: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 8, name: "M R D Perera", hours: 5, money: 500, profilePic: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+  { rank: 5, name: "A R Withana", hours: 10, money: 1000, profilePic: WithanaImage },
+  { rank: 6, name: "K A A Priyanga", hours: 7, money: 700, profilePic: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+  { rank: 7, name: "M R D Perera", hours: 5, money: 500, profilePic: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
 ];
