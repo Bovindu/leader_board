4053030ae423e697e19720a6dc676db955b326c7
hr.info <hr.info@HP-SRVEY2>
2025-06-09 14:53:02 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -4,7 +4,7 @@ import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
   { rank: 1, name: "N K V M P Ariyadasa", hours: 113, money: 11300, profilePic: "" },
-  { rank: 2, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "" },
+  { rank: 2, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749460960/2140404621_kvit40.jpg" },
   { rank: 3, name: "L.B.C. Premathilaka", hours: 47, money: 4700, profilePic: "" },
   { rank: 4, name: "Ranjan Karunanayaka", hours: 33, money: 3300, profilePic: "" },
   { rank: 5, name: "Krishantha Gunasekara", hours: 28, money: 2800, profilePic: "" },
