19932626e179d82c2142e47cd79c4591fdb8d665
hr.info <hr.info@HP-SRVEY2>
2025-06-09 14:35:09 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -3,11 +3,5 @@ import fowzinImage from '../img/kobe.png';
 import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
-  { rank: 1, name: "H H C H Ediriweera", hours: 80, money: 800, profilePic: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 2, name: "D S I Manamperi", hours: 76, money: 1600, profilePic: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 3, name: "A K M Fowzin", hours: 38, money: 3800, profilePic: fowzinImage },
-  { rank: 4, name: "G Wasantha", hours: 31, money: 3100, profilePic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 5, name: "A R Withana", hours: 10, money: 1000, profilePic: WithanaImage },
-  { rank: 6, name: "K A A Priyanga", hours: 7, money: 700, profilePic: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-  { rank: 7, name: "M R D Perera", hours: 5, money: 500, profilePic: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+  { rank: 1, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "" },
 ];
