6824dac5b3eb01a1ea97212b1434640e5403d3b0
hr.info <hr.info@HP-SRVEY2>
2025-06-09 15:03:24 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -3,7 +3,7 @@ import fowzinImage from '../img/kobe.png';
 import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
-  { rank: 1, name: "N K V M P Ariyadasa", hours: 113, money: 11300, profilePic: "" },
+  { rank: 1, name: "N K V M P Ariyadasa", hours: 113, money: 11300, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749461584/2140104339_k00ycg.jpg" },
   { rank: 2, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749460960/2140404621_kvit40.jpg" },
   { rank: 3, name: "L.B.C. Premathilaka", hours: 47, money: 4700, profilePic: "" },
   { rank: 4, name: "Ranjan Karunanayaka", hours: 33, money: 3300, profilePic: "" },
