49de57e212acfa874752cf4aa4ec651c03dfc932
hr.info <hr.info@HP-SRVEY2>
2025-06-09 14:39:52 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -3,6 +3,15 @@ import fowzinImage from '../img/kobe.png';
 import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
-  { rank: 1, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "" },
-  { rank: 2, name: "Ariya Wimalsiri", hours: 8, money: 800, profilePic: "" },
+  { rank: 1, name: "N K V M P Ariyadasa", hours: 113, money: 11300, profilePic: "" },
+  { rank: 2, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "" },
+  { rank: 3, name: "L.B.C. Premathilaka", hours: 47, money: 4700, profilePic: "" },
+  { rank: 4, name: "Ranjan Karunanayaka", hours: 33, money: 3300, profilePic: "" },
+  { rank: 5, name: "Arun Kedhwal", hours: 30, money: 3000, profilePic: "" },
+  { rank: 6, name: "Krishantha Gunasekara", hours: 28, money: 2800, profilePic: "" },
+  { rank: 7, name: "H H T Hewaarachchi", hours: 26, money: 2600, profilePic: "" },
+  { rank: 8, name: "K A Mahinda", hours: 22, money: 2200, profilePic: "" },
+  { rank: 9, name: "Chandani Ediriweera", hours: 12, money: 1200, profilePic: "" },
+  { rank: 10, name: "Muskaan Raghuvanshi", hours: 10, money: 1000, profilePic: "" },
+  { rank: 11, name: "Ariya Wimalsiri", hours: 8, money: 800, profilePic: "" },
 ];
