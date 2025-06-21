62e9ff33151851b35024f188602ea4b772888036
bovin <bovin@DESKTOP-1KSL3R7>
2025-06-21 17:42:37 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -9,5 +9,4 @@ export const leaderboardData = [
   { rank: 4, name: "Ranjan Karunanayaka", hours: 33, money: 3300, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749463090/Ranjab_mc8fy6.jpg" },
   { rank: 5, name: "Krishantha Gunasekara", hours: 29, money: 2900, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749462276/gunasekara_a1iuoq.jpg" },
   { rank: 6, name: "H H T Hewaarachchi", hours: 26, money: 2600, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749462276/heewaracchchi_o04kfk.jpg" },
-  { rank: 7, name: "K A Mahinda", hours: 22, money: 2200, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749462276/mahinda_d8d8rb.jpg" },
 ];
