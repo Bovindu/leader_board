fabd6c4ac1f2ea6557ecb8f29988ca2976ac1356
bovin <bovin@DESKTOP-1KSL3R7>
2025-06-21 19:49:54 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -6,4 +6,6 @@ export const leaderboardData = [
   { rank: 1, name: "N K V M P Ariyadasa", hours: 113, money: 11300, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749461584/2140104339_k00ycg.jpg" },
   { rank: 2, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749460960/2140404621_kvit40.jpg" },
   { rank: 3, name: "L.B.C. Premathilaka", hours: 47, money: 4700, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749463396/premathilaka_vb5vcl.jpg" },
+  { rank: 4, name: "Ranjan Karunanayaka", hours: 33, money: 3300, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749463090/Ranjab_mc8fy6.jpg" },
+  { rank: 5, name: "Krishantha Gunasekara", hours: 29, money: 2900, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749462276/gunasekara_a1iuoq.jpg" },
 ];
