b5973584d810bdc90bfc04a08cc75c2d9a92c48d
bovin <bovin@DESKTOP-1KSL3R7>
2025-06-23 09:31:48 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -3,10 +3,11 @@ import fowzinImage from '../img/kobe.png';
 import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
-  { rank: 1, name: "N K V M P Ariyadasa", hours: 113, money: 11300, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749461584/2140104339_k00ycg.jpg" },
-  { rank: 2, name: "Akhila Withana", hours: 48, money: 4800, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749460960/2140404621_kvit40.jpg" },
-  { rank: 3, name: "L.B.C. Premathilaka", hours: 47, money: 4700, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749463396/premathilaka_vb5vcl.jpg" },
-  { rank: 4, name: "Ranjan Karunanayaka", hours: 33, money: 3300, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749463090/Ranjab_mc8fy6.jpg" },
-  { rank: 5, name: "Krishantha Gunasekara", hours: 29, money: 2900, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1749462276/gunasekara_a1iuoq.jpg" },
-  { rank: 6, name: "Uresh Kadana Ararachi", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750650548/man_ujfxc0.webp" },
+  { rank: 1, name: "Uresh Kadana Ararachi", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750651080/pngwing.com_ed8vhi.png" },
+  { rank: 2, name: "Haritha Prabath", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750651080/pngwing.com_ed8vhi.png" },
+  { rank: 3, name: "Buddhika Chathuranga", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750651080/pngwing.com_ed8vhi.png" },
+  { rank: 4, name: "Chathura Jayawardena", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750651080/pngwing.com_ed8vhi.png" },
+  { rank: 5, name: "Dinuka Sampath", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750651080/pngwing.com_ed8vhi.png" },
+  { rank: 6, name: "Thejan Perera", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750651080/pngwing.com_ed8vhi.png" },
+  { rank: 7, name: "Oshara Weragala", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750651080/pngwing.com_ed8vhi.png" },
 ];
