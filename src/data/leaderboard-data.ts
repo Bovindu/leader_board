4e62466fa879d3955175df00d32e02077b5ce93a
hr.info <hr.info@HP-SRVEY2>
2025-07-01 13:29:33 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -3,14 +3,14 @@ import fowzinImage from '../img/kobe.png';
 import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
-  { rank: 1, name: "Chathura Jayawardana", hours: 77, money: 6440, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695818/chathura_wvacee.jpg" },
-  { rank: 2, name: "Haritha Prabath", hours: 63, money: 5500, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695088/Haritha_h6wrcw.jpg" },
-  { rank: 3, name: "Noyel Nishantha", hours: 59, money: 3420, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694503/noel_iu8vkp.jpg" },
-  { rank: 4, name: "Buddhika Chathuranga", hours: 7, money: 140, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750696164/Buddika_zfwiuk.jpg" },
-  { rank: 5, name: "Indika Premarathna", hours: 6, money: 600, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694053/5634_Indika_mjcdx1.jpg" },
-  { rank: 6, name: "Tharanga Kumara", hours: 6, money: 120, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695844/tharanga_m5aacn.jpg" },
-  { rank: 7, name: "Thejan Perera", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694020/5635_Thejan_h0oyvz.jpg" },
-  { rank: 8, name: "Oshara Weragala", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694418/oshara_cqtluv.jpg" },
-  { rank: 9, name: "Uresh Kadana Arachchi", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695361/5622_Uresh_Pic_nhpwsq.jpg" },
-  { rank: 10, name: "Dinuka Sampath", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750696177/Dinuka_gxnugl.jpg" },
+  { rank: 1, name: "Thejan Perera", hours: 117, money: 2340, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694020/5635_Thejan_h0oyvz.jpg" },
+  { rank: 2, name: "Noyel Nishantha", hours: 97, money: 4180, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694503/noel_iu8vkp.jpg" },
+  { rank: 3, name: "Chathura Jayawardana", hours: 77, money: 6440, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695818/chathura_wvacee.jpg" },
+  { rank: 4, name: "Haritha Prabath", hours: 63, money: 5500, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695088/Haritha_h6wrcw.jpg" },
+  { rank: 5, name: "Oshara Weragala", hours: 54, money: 1080, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694418/oshara_cqtluv.jpg" },
+  { rank: 6, name: "Tharanga Kumara", hours: 16, money: 320, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695844/tharanga_m5aacn.jpg" },
+  { rank: 7, name: "Dinuka Sampath", hours: 15, money: 300, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750696177/Dinuka_gxnugl.jpg" },
+  { rank: 8, name: "Buddhika Chathuranga", hours: 7, money: 140, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750696164/Buddika_zfwiuk.jpg" },
+  { rank: 9, name: "Indika Premarathna", hours: 7, money: 620, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750694053/5634_Indika_mjcdx1.jpg" },
+  { rank: 10, name: "Uresh Kadana Arachchi", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/dlcm7h3zb/image/upload/v1750695361/5622_Uresh_Pic_nhpwsq.jpg" },
 ];
