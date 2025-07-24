92dc6c533a522947f68457ebc3a716d9da41e910
Bovindu <87230990+Bovindu@users.noreply.github.com>
2025-07-24 15:17:51 +0530

    Updated constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const PRICE_CONFIG = {
             POOL_PRICE: 35000,
-            PRICE_PER_HOUR: 100,
+            PRICE_PER_HOUR: 20,
             TOTAL_HOURS: 0,
             } as const; 
             
