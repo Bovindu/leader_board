7d2f84e82f8afa0027e50af855e9bed7c460f5df
hr.info <hr.info@HP-SRVEY2>
2025-08-28 16:53:43 +0530

    Update leaderboard data

--- src/data/leaderboard-data.ts
+++ src/data/leaderboard-data.ts
@@ -3,14 +3,14 @@ import fowzinImage from '../img/kobe.png';
 import WithanaImage from '../img/ad.jpg';
 
 export const leaderboardData = [
-  { rank: 1, name: "Rasika Gayan Sandharuwan", hours: 515, money: 21900, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412010/Rasika_Gayan_Sandharuwan_pb0ngl.jpg" },
-  { rank: 2, name: "Chaminda Anura Kumara", hours: 405, money: 15300, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412009/Chaminda_Anura_Kumara_tcjno0.jpg" },
-  { rank: 3, name: "Ishara Liyanaarachchi", hours: 226, money: 6040, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412008/Ishara_Liyanaarachchi_ludvqh.jpg" },
-  { rank: 4, name: "Lakmal Haputhanthri", hours: 207, money: 5280, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1754020294/Lakmal_z6fwib.png" },
-  { rank: 5, name: "Harin Ranaweera", hours: 189, money: 4560, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412005/Harin_Ranaweera_xi60gw.jpg" },
-  { rank: 6, name: "Pasindu Pubuditha Thennakoon", hours: 177, money: 4080, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412008/5588_E_Pasindu_Pubuditha_kiylzv.jpg" },
-  { rank: 7, name: "Wasana Madhumali Gunarathna", hours: 128, money: 2560, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412005/5583_A_A_Wasana_Madumali_Gunarathna_ynj9lf.jpg" },
-  { rank: 8, name: "Darshana Perera", hours: 18, money: 360, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412011/Darshana_Perera_zz5gij.jpg" },
-  { rank: 9, name: "Chaminda Kumara Senevirathne", hours: 3, money: 60, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412010/Chaminda_Kumara_Senevirathne_osgzlb.jpg" },
+  { rank: 1, name: "Chaminda Anura Kumara", hours: 298, money: 8920, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412009/Chaminda_Anura_Kumara_tcjno0.jpg" },
+  { rank: 2, name: "Rasika Gayan Sandharuwan", hours: 275, money: 8000, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412010/Rasika_Gayan_Sandharuwan_pb0ngl.jpg" },
+  { rank: 3, name: "Harin Ranaweera", hours: 90, money: 1800, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412005/Harin_Ranaweera_xi60gw.jpg" },
+  { rank: 4, name: "Lakmal Haputhanthri", hours: 77, money: 1540, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1754020294/Lakmal_z6fwib.png" },
+  { rank: 5, name: "Ishara Liyanaarachchi", hours: 44, money: 880, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412008/Ishara_Liyanaarachchi_ludvqh.jpg" },
+  { rank: 6, name: "Pasindu Pubuditha Thennakoon", hours: 41, money: 820, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412008/5588_E_Pasindu_Pubuditha_kiylzv.jpg" },
+  { rank: 7, name: "Wasana Madhumali Gunarathna", hours: 5, money: 100, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412005/5583_A_A_Wasana_Madumali_Gunarathna_ynj9lf.jpg" },
+  { rank: 8, name: "Chaminda Kumara Senevirathne", hours: 3, money: 60, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412010/Chaminda_Kumara_Senevirathne_osgzlb.jpg" },
+  { rank: 9, name: "Darshana Perera", hours: 2, money: 40, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412011/Darshana_Perera_zz5gij.jpg" },
   { rank: 10, name: "Duminda Kumara", hours: 0, money: 0, profilePic: "https://res.cloudinary.com/drn4ip7ht/image/upload/v1753412013/Duminda_Kumara_uk9a12.jpg" },
 ];
